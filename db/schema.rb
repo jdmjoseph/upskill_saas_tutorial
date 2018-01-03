6f10cd6cacf74fe306320863c73f03b15edffe54
Joe Worrall <joeworrall38@gmail.com>
2018-01-03 00:32:13 +0000

    Created contacts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171229174838) do
+ActiveRecord::Schema.define(version: 20180103002412) do
 
   create_table "contacts", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.text "comments"
+    t.string   "name"
+    t.string   "email"
+    t.text     "comments"
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null:  false
+    t.datetime "updated_at", null: false
   end
 
 end
