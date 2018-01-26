4c67b4d9ced5ce993b302fcabe2c4aeea30bbbdb
Joe Worrall <joeworrall38@gmail.com>
2018-01-26 02:36:58 +0000

    Fixed bug in users.js, cleaned up code and format in user.rb and registrations controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -6,13 +6,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def create
     super do |resource|
       if params[:plan]
-        resourse.plan_id = params[:plan]
+        resource.plan_id = params[:plan]
         if resource.plan_id == 2
           resource.save_with_subscription
         else
           resource.save
-        end  
-      end  
+        end
+      end
     end
-  end 
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,9 +14,9 @@ class User < ApplicationRecord
   # Store customer.id as the customer token and save the user.
   def save_with_subscription
     if valid?
-      customer = Strip::Customer.create(description: email, plan: plan_id, card: stripe_card_token )
+      customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
       self.stripe_customer_token = customer.id
       save!
     end
   end
-end
+end 
